9206753952bc48c9320ad0642a01f12862a6ebb7
Group1-IAT <66618842+Group1-IAT@users.noreply.github.com>
2020-09-14 18:28:24 +0200

    Initial Setup for WebApplication and WebAPI  for DreamVacationSpot Project

---
+++ WebAPI_DreamVacationSpot(Backend)/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace WebAPIRestaurant_Backend_
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
