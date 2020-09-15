343addf4766f340e06df0c21ca848b0aa497a688
Group1-IAT <66618842+Group1-IAT@users.noreply.github.com>
2020-09-15 11:22:51 +0200

    Few properties added to Tripbooking

--- WebAPI_DreamVacationSpot(Backend)/Models/TripBooking.cs
+++ WebAPI_DreamVacationSpot(Backend)/Models/TripBooking.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebAPIRestaurant_Backend_.Models
+namespace WebAPI_DreamVacationSpot_Backend_.Models
 {
     public class TripBooking
     {
 
         public int TripBookingID { get; set; }
         public  int Date { get; set; }
+
+        public string TripName { get; set; }
+
+       
 
 
     }
