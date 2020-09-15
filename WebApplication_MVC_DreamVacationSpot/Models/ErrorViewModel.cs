66b59a609019fdfbe5a7f10c3db11acbfe7408bc
Group1-IAT <66618842+Group1-IAT@users.noreply.github.com>
2020-09-15 11:14:38 +0200

    WebApplication MVC setup is added

---
+++ WebApplication_MVC_DreamVacationSpot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication_MVC_DreamVacationSpot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
