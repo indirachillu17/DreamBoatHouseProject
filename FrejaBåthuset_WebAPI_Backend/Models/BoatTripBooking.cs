b798aa01596d136d61d10aa9d87d52c0c1e02737
Group1-IAT <66618842+Group1-IAT@users.noreply.github.com>
2020-09-20 17:03:47 +0200

    Added  BoatTripBooking Model Class

---
+++ FrejaBåthuset_WebAPI_Backend/Models/BoatTripBooking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace FrejaBåthuset_WebAPI_Backend.Models
+{
+    public class BoatTripBooking
+    {
+      
+        public int TripID { get; set; }
+
+        public int DiscoverBoatHouse{ get; set; }
+
+        public int DateTime { get; set; }
+
+        public int OtherActivities { get; set; }
+
+
+    }
+}
