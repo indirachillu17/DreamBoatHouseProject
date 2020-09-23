c9d81aba67c9ec487bfbed3945ccfec58cf866cc
Group1-IAT <66618842+Group1-IAT@users.noreply.github.com>
2020-09-23 18:23:51 +0200

    I have done the Web Application ,WebAPI , SQl lite setup .Also i have added the model class in WebAPI project .

---
+++ FrejyaBåtHuset_WebAPI_Backend/Migrations/20200923112643_InitialCreate1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FrejyaBåtHuset_WebAPI_Backend.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BåtHusetBokning",
+                columns: table => new
+                {
+                    BåtHusetBokningID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DiscoverBoatHouse = table.Column<int>(nullable: false),
+                    BoatStart = table.Column<DateTime>(nullable: false),
+                    BoatEnd = table.Column<DateTime>(nullable: false),
+                    ActivitiesTiming = table.Column<string>(nullable: true),
+                    OtherActivities = table.Column<string>(nullable: true),
+                    Restaurant = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BåtHusetBokning", x => x.BåtHusetBokningID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BåtHusetBokning");
+        }
+    }
+}
---
+++ FrejyaBåtHuset_WebAPI_Backend/Models/Användare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrejyaBåtHuset_WebAPI_Backend.Models
+{
+    public class Användare
+    {
+
+        public int AnvändareID { get; set; }
+        public string UserName { get; set; }
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}
