c28c28f39d7fcec6b3f6ad05738a5d92730a6cac
Nayana Sharma <nayanapsharma@gmail.com>
2020-09-23 21:25:54 +0200

    Revert "I just created login class with propetis."
    This reverts commit 40470b90b0b58402272c0eee413ed00ccdef1692.

--- FrejaBåthuset_WebAPI_Backend/Models/BoatTripBooking.cs
+++ FrejaBåthuset_WebAPI_Backend/Models/BoatTripBooking.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
-namespace FreyjaBåtHuset_API_Backend.Models
+namespace FrejaBåthuset_WebAPI_Backend.Models
 {
     public class BoatTripBooking
     {
-
+      
         public int BoatTripBookingID { get; set; }
 
-        public int DiscoverBoatHouse { get; set; }
+        public int DiscoverBoatHouse{ get; set; }
 
         //public DateTime DateTime { get; set; }
 
         public string OtherActivities { get; set; }
+
+
     }
 }
