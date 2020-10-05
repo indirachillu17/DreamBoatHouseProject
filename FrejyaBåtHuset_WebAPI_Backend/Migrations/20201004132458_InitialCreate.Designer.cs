f632579bb9fa4a32b32d000fb04cf436290b384b
TaraEng <63234768+TaraEng@users.noreply.github.com>
2020-10-05 09:16:57 +0200

    Webpage changes

--- FrejyaBåtHuset_WebAPI_Backend/Migrations/20201004132458_InitialCreate.Designer.cs
+++ FrejyaBåtHuset_WebAPI_Backend/Migrations/20201004132458_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using FrejyaBåtHuset_WebAPI_Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FrejyaBåtHuset_WebAPI_Backend.Migrations
 {
     [DbContext(typeof(FrejyaBåtHuset_WebAPI_BackendContext))]
-    partial class FrejyaBåtHuset_WebAPI_BackendContextModelSnapshot : ModelSnapshot
+    [Migration("20201004132458_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FrejyaBåtHuset_WebAPI_Backend/Migrations/20201004132458_InitialCreate.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FrejyaBåtHuset_WebAPI_Backend.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailId",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Andraaktiviteter",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailId",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Andraaktiviteter",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
