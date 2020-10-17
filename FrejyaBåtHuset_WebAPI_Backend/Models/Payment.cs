0e9e26a1185b962fb3fcb40ee58eeedaec1ac7f2
Nayana Sharma <nayanapsharma@gmail.com>
2020-10-17 19:29:56 +0200

    Added payment

---
+++ FrejyaBåtHuset_WebAPI_Backend/Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrejyaBåtHuset_WebAPI_Backend.Models
+{
+    public class Payment
+    {
+        public int CardNo { get; set; }
+        public string ExpiryDate { get; set; }
+        public int SecretNum { get; set; }
+
+    }
+}
